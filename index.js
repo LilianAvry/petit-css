80cbcc6d62b417b0a83ceeb5955c530a747872ad
lilianchretienne <dev@lil.re>
2021-09-28 15:08:39 +0200

    Update build script

--- index.js
+++ index.js
@@ -7,16 +7,19 @@ function read (name, index) {
   const path = `${__dirname}/src/${name.toLowerCase()}/style.css`;
   const options = { encoding:'utf8', flag:'r' };
   const content = fs.readFileSync(path, options);
-  // console.log(content.split('\n')[0])
-
+  const lines = content.split('\n')
+  const finalContent = lines
+    .filter(line => !line.includes('@import'))
+    .join('\n')
   const comment = index === 0
     ? `/* ${name} -------------------------------- */\r\n`
     : `\r\n/* ${name} -------------------------------- */\r\n`;
-  const data = comment + content;
+
+  const data = comment + finalContent;
   fs.appendFileSync(petitPath, data);
 }
 
-const components = ['Button', 'Card', 'Code', 'Form', 'Grid', 'List', 'Navigation', 'Table', 'Typography'];
+const components = ['Colors', 'Button', 'Card', 'Code', 'Form', 'Grid', 'List', 'Navigation', 'Table', 'Typography'];
 
 for (let i = 0; i < components.length; i++) {
  read(components[i], i)
